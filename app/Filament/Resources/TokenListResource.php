9457302e4ae141b34d6cacbd8d84c5ff91990cd3
Fatan Akbar Yogenta <fatan.dev@gmail.com>
2024-10-21 22:09:05 +0700

    nav group token list

--- app/Filament/Resources/TokenListResource.php
+++ app/Filament/Resources/TokenListResource.php
@@ -18,7 +18,7 @@ class TokenListResource extends Resource
     protected static ?string $model = TokenList::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
+    protected static ?string $navigationGroup = 'API';
     public static function form(Form $form): Form
     {
         return $form
