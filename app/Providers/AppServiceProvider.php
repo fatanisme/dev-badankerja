1016330ae9b3af0308aaa443ca19f67926e069f3
Fatan Akbar Yogenta <fatan.dev@gmail.com>
2024-10-22 12:07:42 +0700

    qwe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
-        \URL::forceScheme('https');
+        URL::forceScheme('https');
     }
 }
